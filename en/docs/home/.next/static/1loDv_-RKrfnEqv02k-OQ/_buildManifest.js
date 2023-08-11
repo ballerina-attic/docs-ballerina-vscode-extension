a8e979d657f00b25d3fc205cb67df9ed692695ba
praneesha <praneesha@wso2.com>
2023-08-11 16:48:22 +0530

    Fix the VS Code home page broken links

--- en/docs/home/.next/static/1loDv_-RKrfnEqv02k-OQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/952-f25dba3e1c2fcd28.js","static/css/4ab724a571b4505a.css","static/chunks/pages/index-ea77f0589f8b245e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
